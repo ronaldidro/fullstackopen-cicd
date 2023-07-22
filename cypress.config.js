3208bbd6ab1116f97e1343d3aeec7a4f2a40eae0
ronaldidro <rdiaz@digital.gob.pe>
2023-07-21 22:17:10 -0500

    removed cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,17 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on) {
+    setupNodeEvents() {
       // implement node event listeners here
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        console.log(launchOptions.args)
-
-        if (browser.name === 'chrome') {
-          launchOptions.args.push('--disable-gpu')
-        }
-
-        return launchOptions
-      })
     },
   },
 })
